340ffa890a403be9fbccda4423f1563b7a4dcdce
VMB-DEV <vkdev7360@gmail.com>
2026-03-08 20:01:39 +0100

    clean folder creation for frames

--- gif-ascii/getFrames.sh
+++ gif-ascii/getFrames.sh
@@ -1,4 +1,5 @@
 # extract all the frame from the video $1 to the folder $2
+# $ sh getFrames.sh video.mp4 framesFolder
 
 if [ -z "$1" ] || [ -z "$2" ]; then
   echo "need 2 argument to run"
@@ -7,5 +8,8 @@ if [ -z "$1" ] || [ -z "$2" ]; then
   return
 fi
 
-ffmpeg -i "$1" "$2/${1%.*}_frame_%04d.png"
+VIDEO_NAME="${1%.*}"
+VIDEO_FRAME_FOLDER="${VIDEO_NAME}_frame_png"
+mkdir "$2/$VIDEO_FRAME_FOLDER"
+ffmpeg -i "$1" "$2/${VIDEO_FRAME_FOLDER}_frame_%04d.png"
 
